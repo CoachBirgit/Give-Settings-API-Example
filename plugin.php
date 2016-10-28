320eca293e4978f834c1ec64fef3831f09cb3a09
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-28 10:22:31 +0530

    Update plugin header information

--- plugin.php
+++ plugin.php
@@ -1,11 +1,42 @@
 <?php
 /**
- * Plugin Name:        Give - Setting API Fields Example
- * Description:        Learn how to add setting to plugin setting page or Form data metabox with given plugin filter and action.
- * Author:             WordImpress
- * Author URI:         http://wordimpress.com
- * Contributors:       WordImpress
- * Version:            1.0
+ * Plugin Name: Give - Setting API Fields Example
+ * Plugin URI:
+ * Description: Learn how to add custom setting for donation form and plugin settings.
+ * Author: WordImpress
+ * Author URI: https://wordimpress.com
+ * Version: 1.0
+ * Text Domain:
+ * Domain Path: /languages
+ * GitHub Plugin URI:
+ *
+ * Give - Setting API Fields Example is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Give - Setting API Fields Example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Give - Setting API Fields Example. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * A Tribute to Open Source:
+ *
+ * "Open source software is software that can be freely used, changed, and shared (in modified or unmodified form) by anyone. Open
+ * source software is made by many people, and distributed under licenses that comply with the Open Source Definition."
+ *
+ * -- The Open Source Initiative
+ *
+ * Give - Setting API Fields Example is a tribute to the spirit and philosophy of Open Source. We at WordImpress gladly embrace the Open Source philosophy both
+ * in how Give - Setting API Fields Example itself was developed, and how we hope to see others build more from our code base.
+ *
+ * Give - Setting API Fields Example would not have been possible without the tireless efforts of WordPress and the surrounding Open Source projects and their talented developers. Thank you all for your contribution to WordPress.
+ *
+ * - The WordImpress Team
+ *
  */
 
 /**
