cc541ae1bd5469ad8d6f0a265d0f668a38f7f449
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-28 10:20:18 +0530

    Fix: missing metabox settings

--- plugin.php
+++ plugin.php
@@ -5,22 +5,44 @@
  * Author:             WordImpress
  * Author URI:         http://wordimpress.com
  * Contributors:       WordImpress
- * Version:            0.1
+ * Version:            1.0
  */
+
+/**
+ * Add custom core plugin setting.
+ *
+ * @since 1.0
+ *
+ * @param $settings
+ *
+ * @return array
+ */
+function give_add_setting_api_fields_settings( $settings ) {
+	// Bootstrap.
+	require_once dirname( __FILE__ ) . '/includes/class-setting-api-fields.php';
+
+	$settings[] = new Give_Setting_API_Fields();
+
+	return $settings;
+}
 
 
 /**
- * Initialize new setting.
+ * Load custom metabox and core settings.
  *
- * @param  array $settings
- * @return array
+ * @since 1.0
  */
-function give_add_setting_api_fields_settings( $settings ){
-	// Bootstrap.
-	require_once dirname( __FILE__ ) . '/includes/class-setting-api-fields.php';
+function give_setting_api_example_init() {
+	if ( ! class_exists( 'Give' ) ) {
+		return;
+	}
+
+	// Load metabox settings.
 	require_once dirname( __FILE__ ) . '/includes/class-metabox-setting.php';
 
-	$settings[] = new Give_Setting_API_Fields();
-	return $settings;
+	// Load custom core settings.
+	add_filter( 'give-settings_get_settings_pages', 'give_add_setting_api_fields_settings' );
 }
-add_filter( 'give-settings_get_settings_pages', 'give_add_setting_api_fields_settings' );
+
+add_action( 'plugins_loaded', 'give_setting_api_example_init' );
+
